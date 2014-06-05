28f1794ae489f0edde75eff92080f6e9c32bb919
ikenney <ian.kenney@lostsock.co.uk>
2014-06-05 07:38:12 +0100

    Update dot_functions.sh
    remove colours from messages

--- bin/dot_functions.sh
+++ bin/dot_functions.sh
@@ -1,36 +1,28 @@
-# colors
-
-red=$(tput setaf 1)
-green=$(tput setaf 2)
-yellow=$(tput setaf 3)
-normal=$(tput sgr0)
-
 # Print functions
 
 print_padded() {
-  len=${#2}
-  printf "%-50s [$3%$((len+(10-len)/2))s%$(((10-len)/2))s$normal]\n" "$1" "$2"
+  echo "$1 : $2"
 }
 
 create_notice() {
-  print_padded "Creating $1" "$2" $green
+  echo "Creating $1 : $2"
 }
 
 
 skip_notice() {
-  print_padded "${yellow}Skipping${normal} $1" "$2" $yellow
+  echo "Skipping $1 : $2"
 }
 
 copy_notice() {
-  print_padded "${green}Copying${normal}  $1" "$2" $green
+  echo "Copying $1 : $2"
 }
 
 link_notice() {
-  print_padded "${green}Linking${normal}  $1" "$2" $green
+  echo "Linking $1 : $2"
 }
 
 remove_notice() {
-  print_padded "${red}Removing${normal} $1" "$2" $red
+  echo "Removing $1 : $2"
 }
 
 # Other functions
@@ -38,14 +30,14 @@ remove_notice() {
 directory_warning() {
   name=$(basename $0)
   if [[ ! "$PWD/bin/$name" -ef "$0" ]]; then
-    echo "${yellow}Please run '$name' from dotmatrix root folder${normal}"
+    echo "Please run '$name' from dotmatrix root folder"
     exit 1
   fi
 }
 
 dirty_warning() {
   if [ -n "$(git status --porcelain)" ]; then
-    echo "${red}ERROR: You have a dirty working copy.${normal}"
+    echo "ERROR: You have a dirty working copy."
     echo "Commit or clean any changes, and run bin/upgrade again."
     exit 1
   fi
